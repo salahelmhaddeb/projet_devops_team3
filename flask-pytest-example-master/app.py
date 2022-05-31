413a9cc6d055b204ed813068b0759a59437bc0da
salahelmhaddeb <104069261+salahelmhaddeb@users.noreply.github.com>
2022-05-31 15:19:04 +0200

    Update app.py

--- flask-pytest-example-master/app.py
+++ flask-pytest-example-master/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_pytest_example.handlers.routes import configure_routes
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 configure_routes(app)
 
