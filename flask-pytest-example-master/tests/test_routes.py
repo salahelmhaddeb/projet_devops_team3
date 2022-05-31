a04849d9905df61dcc4afc074e48c5899f5e5155
salahelmhaddeb <104069261+salahelmhaddeb@users.noreply.github.com>
2022-05-31 10:55:30 +0200

    Update test_routes.py

--- flask-pytest-example-master/tests/test_routes.py
+++ flask-pytest-example-master/tests/test_routes.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import json
 
-from handlers.routes import configure_routes
+from flask-pytest-example-master/handlers/routes.py import configure_routes
 
 
 def test_base_route():
